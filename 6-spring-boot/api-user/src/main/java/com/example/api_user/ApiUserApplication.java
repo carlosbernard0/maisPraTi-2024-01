96d79dafa1a388f52dd1dfe607f6ea370a9b5bdb
carlosbernard0 <carlosbernardo774@gmail.com>
2024-11-02 11:19:06 -0300

    test commit

--- 6-spring-boot/api-user/src/main/java/com/example/api_user/ApiUserApplication.java
+++ 6-spring-boot/api-user/src/main/java/com/example/api_user/ApiUserApplication.java
@@ -2,8 +2,10 @@ package com.example.api_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ApiUserApplication {
 
 	public static void main(String[] args) {
--- 6-spring-boot/api-user/src/main/java/com/example/api_user/controller/AuthController.java
+++ 6-spring-boot/api-user/src/main/java/com/example/api_user/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.example.api_user.controller;
 
 // Importações necessárias para manipulação de autenticação e JWT (Json Web Token)
 import ch.qos.logback.core.net.SMTPAppenderBase;
+import com.example.api_user.dto.LogiByIdDTO;
 import com.example.api_user.dto.LoginDTO;
 import com.example.api_user.security.JwtTokenProvider;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -60,7 +61,32 @@ public class AuthController {
         } catch (AuthenticationException error) {
             // Se houver uma exceção de autenticação, significa que as credenciais são inválidas.
             // A exceção será capturada e uma RuntimeException será lançada com a mensagem "Invalid Credentials".
+            throw new RuntimeException("Invalid Credentials", error);
+        }
+    }
+
+    @PostMapping("/loginId")
+    public String loginById(@RequestBody LogiByIdDTO LogiByIdDTO) {
+        try {
+            // O AuthenticationManager realiza a autenticação baseada no nome de usuário e senha.
+            // A autenticação é feita criando um UsernamePasswordAuthenticationToken com as credenciais fornecidas (username e password).
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(LogiByIdDTO.getId(), LogiByIdDTO.getPassword())
+            );
+
+            // Se a autenticação for bem-sucedida, o objeto `authentication` conterá as informações do usuário autenticado.
+            // O método `getPrincipal()` retorna o objeto principal da autenticação, que no caso é um `UserDetails` (detalhes do usuário autenticado).
+            UserDetails user = (UserDetails) authentication.getPrincipal();
+
+            // O JwtTokenProvider gera um token JWT usando as informações do usuário autenticado.
+            return jwtTokenProvider.generateToken(user);
+
+        } catch (AuthenticationException error) {
+            // Se houver uma exceção de autenticação, significa que as credenciais são inválidas.
+            // A exceção será capturada e uma RuntimeException será lançada com a mensagem "Invalid Credentials".
             throw new RuntimeException("Invalid Credentials");
         }
+
+
     }
 }
